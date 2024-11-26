c19def0354d9da5427fa3d5f0cb10ca00c95b72b
houghtonrin <112127182+houghtonrin@users.noreply.github.com>
2024-11-26 12:12:58 -0700

    final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let string = 'Rin Houghton'
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
